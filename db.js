faaa7ac56ced2e8a86a05323060ef833dc56aa34
manminder singh <bewithme2407@gmail.com>
2024-07-04 13:31:59 +0530

    add

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
-dotenv.config();
 
-mongoose.connect(process.env.MONGO, {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+console.log(process.env);
 
 const cardSchema = new mongoose.Schema({
   name: String,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoute = require('../server/Routes/user')
+const userRoute = require('./Routes/user')
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,5 +9,5 @@ app.use(cors());
 app.use('/api/user', userRoute)
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
